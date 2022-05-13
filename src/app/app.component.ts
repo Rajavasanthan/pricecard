0b0744f689e8eeb49951fd4b5fb4b5b9c38e9c64
rajavasanthan <rajavasanthan@gmail.com>
2022-05-13 11:42:06 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,76 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pricecard';
+  data = [
+    {
+      title : "Free",
+      price : 0,
+      isDefault : false,
+      features : [
+        {
+          name : "10 users included",
+          isEnable : true
+        },
+        {
+          name : "2 GB of storage",
+          isEnable : true
+        },
+        {
+          name : "Email support",
+          isEnable : true
+        },
+        {
+          name : "Help center access",
+          isEnable : true
+        }
+      ]
+    },
+    {
+      title : "Plus",
+      price : 20,
+      isDefault : true,
+      features : [
+        {
+          name : "20 users included",
+          isEnable : true
+        },
+        {
+          name : "10 GB of storage",
+          isEnable : true
+        },
+        {
+          name : "Priority Email support",
+          isEnable : true
+        },
+        {
+          name : "Help center access",
+          isEnable : true
+        }
+      ]
+    },
+    {
+      title : "Pro",
+      price : 40,
+      isDefault : false,
+      features : [
+        {
+          name : "30 users included",
+          isEnable : true
+        },
+        {
+          name : "15 GB of storage",
+          isEnable : true
+        },
+        {
+          name : "Phone and email support",
+          isEnable : true
+        },
+        {
+          name : "Help center access",
+          isEnable : true
+        }
+      ]
+    },
+  ]
+  value=""
 }
---
+++ src/app/price-card/price-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-card',
+  templateUrl: './price-card.component.html',
+  styleUrls: ['./price-card.component.css']
+})
+export class PriceCardComponent implements OnInit {
+  @Input() priceData:any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
